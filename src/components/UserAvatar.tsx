7098a16abc1da50082a2faebddcaba5c7f6678fd
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-21 21:22:38 +0100

    pushing

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import img from "../components/images/img.jpg"
+import img from "../components/images/img.jpg";
 interface UserAvatarProps {
   name: string;
   imageUrl: string;
@@ -8,17 +8,17 @@ interface UserAvatarProps {
 const UserAvatar: React.FC<UserAvatarProps> = ({ name, imageUrl }) => {
   return (
     <div className="flex items-center space-x-2 rounded-lg px-2 py-0.5 shadow-sm bg-white">
-    <Image 
-      src={img}
-      alt={`${name}'s Avatar`} 
-      width={28} 
-      height={28} 
-      className="rounded-full object-cover"
-    />
-    <span className="text-black font-bold text-sm whitespace-nowrap">{name}</span>
-  </div>
-  
-  
+      <Image
+        src={img}
+        alt={`${name}'s Avatar`}
+        width={28}
+        height={28}
+        className="rounded-full object-cover"
+      />
+      <span className="text-black font-bold text-sm whitespace-nowrap">
+        {name}
+      </span>
+    </div>
   );
 };
 
