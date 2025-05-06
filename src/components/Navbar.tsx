f4bb053ceb31d67e2018f7904f866c4d79fce683
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-05-06 14:19:12 +0100

    hellp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-white shadow px-6 py-4 fixed w-full top-0 z-50 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-black-900">
-        <div className="text-2xl font-bold flex items-center gap-2">
+        <div className="text-2xl font-bold flex items-center gap-2 hover:bg-amber-50 cursor-pointer">
           WhatBytes
         </div>
       </h1>
